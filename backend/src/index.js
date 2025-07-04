87fa13df371619679f5dccd272689f26845adadd
karan96108 <karanjuneja96108@gmail.com>
2025-07-04 09:30:27 +0530

    deployment fix v2

--- backend/src/index.js
+++ backend/src/index.js
@@ -47,7 +47,8 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  // Use a regex to only match non-API routes
+  app.get(/^\/(?!api).*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
