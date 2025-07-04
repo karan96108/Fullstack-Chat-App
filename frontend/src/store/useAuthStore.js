84faff6371e64d99d5520ccb2f950e7704ffe239
karan96108 <karanjuneja96108@gmail.com>
2025-07-04 09:15:58 +0530

    Minor fixes

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import {axiosInstance} from '../lib/axios.js';
 import toast from "react-hot-toast";
 // import { signup } from '../../../backend/src/controllers/auth.controller.js';
 import {io} from 'socket.io-client';
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5050" : "/";
 export const useAuthStore = create((set, get) => ({
   authUser: null,
     isSigningUp : false,
